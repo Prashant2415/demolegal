a686a4411949e5b434c3b3d288682db73e9686b8
Prashant2415 <84087956+Prashant2415@users.noreply.github.com>
2023-03-23 19:50:55 +0530

    Delete legalportal directory

--- legalportal/src/main/java/com/portal/legalportal/repository/LoginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.portal.legalportal.repository;
-
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.portal.legalportal.model.Login;
-@Repository
-public interface LoginRepository extends JpaRepository<Login,String>{
-
-}
